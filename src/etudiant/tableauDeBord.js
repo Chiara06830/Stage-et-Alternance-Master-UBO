84cf788d807c28db00d031e04619f7513146bd49
Chiara <chiara.relevat@oulook.fr>
2020-10-12 18:48:42 +0200

    embelissement tableau

--- src/etudiant/tableauDeBord.js
+++ src/etudiant/tableauDeBord.js
@@ -12,9 +12,9 @@ class Tableau extends React.Component{
         for(let i=0; i<this.nbCol-2; i++){
             infos += "<td> </td>";
         }
-        infos += "<td/> <img src=\"/img/crayon_376363.png\" alt=\"bouton modifier\" width=\"20\" height=\"20\">";
-        infos += "<td/> <img src=\"/img/trash-bin-symbol_318-10194.jpg\" alt=\"bouton modifier\" width=\"20\" height=\"20\">";
-        return <tr dangerouslySetInnerHTML={{ __html: infos }}/>; 
+        infos += "<td style=\"text-align: center\"/> <img src=\"/img/crayon_376363.png\" alt=\"bouton modifier\" width=\"20\" height=\"20\">";
+        infos += "<td style=\"text-align: center\"/> <img src=\"/img/csm_Accroche_dechets_menagers_28360027d4.png\" alt=\"bouton modifier\" width=\"20\" height=\"20\">";
+        return <tr dangerouslySetInnerHTML={{ __html: infos }}/>;
     }
 
     createTable(nbColonne, tab){
@@ -22,10 +22,10 @@ class Tableau extends React.Component{
         this.nbCol = nbColonne;
         for(let i=0; i<nbColonne; i++){
             let nom = tab[i];
-            col += "<td>" + nom + "</td>";
+            col += "<th>" + nom + "</th>";
         }
 
-        return <tr dangerouslySetInnerHTML={{ __html: col }}/>;
+        return <tr class="titre" dangerouslySetInnerHTML={{ __html: col }}/>;
     }
 
     render(){
